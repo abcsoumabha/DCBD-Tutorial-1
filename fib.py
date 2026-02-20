288b902d42f91e28e015207033f764067beb56a7
Blaze-Leo <guptaanurag2005@gmail.com>
2026-02-20 16:29:36 +0530

    Update fib.py

--- fib.py
+++ fib.py
@@ -1,14 +1,14 @@
-def fibonacci(n):
-    a, b = 0, 1
+def tribonacci(n):
+    a, b, c = 0, 1, 1
     for _ in range(n):
-        a, b = b, a + b
+        a, b, c = b, c, a + b + c
     return a
 
 
 def main():
     n = int(input("Enter n: "))
-    result = fibonacci(n)
-    print(f"Fibonacci({n}) = {result}")
+    result = tribonacci(n)
+    print(f"Tribonacci({n}) = {result}")
 
 
 if __name__ == "__main__":
